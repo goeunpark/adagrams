198e3487b8b2a2782b4cf81ab41a96b182fd5fe0
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-14 16:55:26 -0700

    adds points scoring word regardless of case and null input

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -5,9 +5,14 @@ POOL = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4
 
 SCORE_CHART = { A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10}
 
-
 def score_word(word)
-  # return points
+  total_points = []
+  SCORE_CHART.each {|letter,letter_points|
+    if word.downcase.include?(letter.to_s.downcase)
+      total_points << letter_points
+    end
+  }
+  return total_points.sum
 end
 
 def uses_available_letters?(input, letters_in_hand)
@@ -38,3 +43,5 @@ def generate_letter_array(pool)
   end
   return letter_array
 end
+
+score_word('DOG')
