5a562c848048e8a1e7a7e7138bf97839d4255b0a
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-16 16:20:38 -0700

    removed unnessary lines in code and adding spacing for readablity

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,5 +1,3 @@
-require 'awesome_print'
-require 'pry'
 require 'csv'
 
 POOL = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
@@ -56,6 +54,7 @@ end
 
 def score_word(word)
   total_points = []
+
   SCORE_CHART.each { |letter, letter_points|
     word.each_char do |char|
       if char.downcase.include?(letter.to_s.downcase)
@@ -70,7 +69,6 @@ end
 
 def uses_available_letters?(input, letters_in_hand)
   input = input.chars
-
   (input & letters_in_hand) == input ? true : false
 end
 
@@ -92,5 +90,3 @@ def generate_letter_array(pool)
   end
   return letter_array
 end
-
-uses_available_letters?('CAT', ['A', 'B', 'C', 'T', 'S'])
