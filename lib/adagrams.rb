1789a25461ad35d59b255a610d300e931580e0dc
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-17 10:54:37 -0700

    Refactor highest_score_from and create two more helper methods to find_all_scores and find_max_words

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,10 +1,8 @@
-require 'pry'
 require 'csv'
 
 POOL = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
 
 SCORE_CHART = { A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 }
-
 
 def is_in_english_dict?(input)
   CSV.open("assets/dictionary-english.csv", "r").each do |word|
@@ -13,23 +11,18 @@ def is_in_english_dict?(input)
   return false
 end
 
-
 def highest_score_from(unscored_words)
-  all_scores = {}
+  all_scores = find_all_scores(unscored_words)
 
-  unscored_words.each do |word|
-    all_scores[word] = score_word(word)
-  end
+  max_scored_words = find_max_words(all_scores)
 
-  max_words = all_scores.select { |word, score| score == all_scores.values.max }
+  first_ten_lettered_word = find_first_ten_lettered_word(max_scored_words)
+  first_min_word = find_first_min_word(max_scored_words)
 
-  first_ten_lettered_word = find_first_ten_lettered_word(max_words)
-  first_min_word = find_first_min_word(max_words)
-
-  any_ten_lettered_word?(max_words) ?
+  any_ten_lettered_word?(max_scored_words) ?
   winning_word = first_ten_lettered_word : winning_word = first_min_word
 
-  first_word_score = max_words.values[0]
+  first_word_score = max_scored_words.values[0]
 
   best_word = {
     word: winning_word,
@@ -41,7 +34,7 @@ def highest_score_from(unscored_words)
 end
 
 def any_ten_lettered_word?(max_words)
-max_words.any? { |word, score| word.length == 10 }
+  max_words.any? { |word, score| word.length == 10 }
 end
 
 def find_first_min_word(max_words)
@@ -52,6 +45,19 @@ end
 def find_first_ten_lettered_word(max_words)
   first_ten_lettered_word = max_words.select { |word, score| word.length == 10 }.keys[0]
   return first_ten_lettered_word
+end
+
+def find_max_words(all_scores)
+  all_scores.select { |word, score| score == all_scores.values.max }
+end
+
+def find_all_scores(unscored_words)
+  all_scores = {}
+
+  unscored_words.each do |word|
+    all_scores[word] = score_word(word)
+  end
+  return all_scores
 end
 
 def score_word(word)
